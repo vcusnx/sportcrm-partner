f852f4ed257f2c8ea1e7df5660a3a626d0e4c6ee
vcusnx <vcusnx@gmail.com>
2024-08-05 19:17:01 +0300

    .

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -22,7 +22,7 @@
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(formData.value),
+                body: JSON.stringify(formData),
             });
 
             const data = await response.json();
