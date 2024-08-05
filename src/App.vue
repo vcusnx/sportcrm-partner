87a0e15e79ca28b0fc0716341e868dd7f4b26daf
vcusnx <vcusnx@gmail.com>
2024-08-05 19:38:10 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
   import WebApp from '@twa-dev/sdk';
-  import Registration from './views/Registration.vue';
+  import Dashboard from './views/Dashboard.vue';
 
   console.log(WebApp.initData)
 </script>
 
 <template>
-  <Registration />
+  <Dashboard />
 </template>
 
 <style scoped></style>
