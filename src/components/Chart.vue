55321259a1d18cca0468471e711a516ff6b8bb5b
vcusnx <vcusnx@gmail.com>
2024-08-08 10:27:52 +0300

    .

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,42 +1,48 @@
 <script lang="ts">
+    import { ref, watch } from 'vue';
     import { Doughnut } from 'vue-chartjs';
     import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js';
     import { usePartnerStore } from '../stores/partner';
 
     ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 
-    const partnerStore = usePartnerStore();
-
-    const withdraw = partnerStore.sum;
-    const debt = partnerStore.debt;
-    const wait = partnerStore.wait;
-
     export default {
         name: "Средства",
         components: { Doughnut },
-        data() {
-            return {
-                chartData: {
-                    labels: ["К выводу", "Должники", "Ожидание"],
-                    datasets: [{
-                        backgroundColor: ["#0070F0", "#ED5959", "#C6C6C6"],
-                        data: [withdraw, debt, wait],
-                        borderWidth: 0
-                    }]
-                },
-                chartOptions: {
-                    responsive: true,
-                    cutout: '70%',
-                    plugins: {
-                        legend: {
-                            display: false
-                        },
-                        title: {
-                            display: false
-                        }
+        setup() {
+            const partnerStore = usePartnerStore();
+
+            const chartData = ref({
+                labels: ["К выводу", "Должники", "Ожидание"],
+                datasets: [{
+                    backgroundColor: ["#0070F0", "#ED5959", "#C6C6C6"],
+                    data: [partnerStore.sum, partnerStore.debt, partnerStore.wait],
+                    borderWidth: 0
+                }]
+            });
+
+            const chartOptions = ref({
+                responsive: true,
+                cutout: '70%',
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: false
                     }
                 }
-            }
+            });
+
+            // Обновление данных диаграммы при изменении данных в partnerStore
+            watch([partnerStore.sum, partnerStore.debt, partnerStore.wait], ([newSum, newDebt, newWait]) => {
+                chartData.value.datasets[0].data = [newSum, newDebt, newWait];
+            });
+
+            return {
+                chartData,
+                chartOptions
+            };
         }
     }
 </script>
